8e0973230e04990e05691a42152780df0498bab0
adam ling <997506412@qq.com>
2018-09-30 11:00:33 +0800

    201809301100

--- apps/controllers/LoginController.php
+++ apps/controllers/LoginController.php
@@ -16,7 +16,24 @@ class LoginController extends Controller
     }
 
     public function postAction(){
-        $this->session->set('user','test');
-        $this->response->redirect('index');
+        $postData  = $_POST;
+        if(isset($_POST['email'])&&$_POST['password']){
+            $user = User::findFirst([
+                'conditions' => [
+                    'email' => $postData['email']
+                ]
+            ]);
+            if($user){
+                if($user->password==md5($_POST['password'])){
+                    $this->session->set('user',$user->getId());
+                }
+            }else{
+                $this->response->redirect('login');
+            }
+        }else{
+            $this->response->redirect('login');
+        }
+        //$this->session->set('user','test');
+        //$this->response->redirect('index');
     }
 }
