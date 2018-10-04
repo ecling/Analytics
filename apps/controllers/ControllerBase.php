3c001fd5a0542715e9d927089442817a0d5ca298
adam ling <997506412@qq.com>
2018-10-05 00:09:14 +0800

    20181005

--- apps/controllers/ControllerBase.php
+++ apps/controllers/ControllerBase.php
@@ -8,13 +8,12 @@ class ControllerBase extends Controller
     public function beforeExecuteRoute()
     {
         //测试去除登录验证
-        /*
         if($this->session->has('user')){
 
         }else{
             $this->response->redirect('login');
             return;
         }
-        */
+
     }
 }
